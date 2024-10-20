e11642a3fbb175aa83125f1c90bb12d9f8c73971
Filip-Chromek <filip.chromek@binconf.com>
2024-10-20 14:25:32 +0200

    auth responsiveness

--- frontend/src/layouts/AuthLayout.vue
+++ frontend/src/layouts/AuthLayout.vue
@@ -1,26 +1,42 @@
-<template>
-  <div class="row" style="height: 100vh; background: linear-gradient(lightcyan,lightblue)">
+9<template>
+  <div class="q-pa-md row-container" style="height: 100vh; background: linear-gradient(lightcyan, lightblue)">
     <div class="col-4 flex flex-center" style="border: 1px solid black;">
-      <router-view/>
+      <router-view />
     </div>
-    <q-separator vertical/>
-    <div class="col flex flex-center"  style="border: 1px solid black; ">
     
-        <img class="obrazok" src="~assets/quasar-logo-vertical.svg">
-      
+    <q-separator vertical class="separator"/>
+
+    <div class="col flex flex-center" style="border: 1px solid black;">
+      <img class="obrazok" src="~assets/quasar-logo-vertical.svg" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
 <style scoped>
+.row-container {
+  display: flex;
+  justify-content: space-between;
+}
 
-  .obrazok{
+.obrazok {
+  height: 90vh;
+}
 
-    height: 90vh;
-    
+@media (max-width: 1100px) {
+  .row-container {
+    flex-direction: column;
+    min-height: 150vh;
   }
+
+  .separator {
+    display: none;
+  }
+
+  .obrazok {
+    height: 50vh; /* Adjust image size on small screens */
+  }
+}
 </style>
